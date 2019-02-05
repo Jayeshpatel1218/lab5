0d380797938d5502317da7ec9707164025df5dcb
Jayeshpatel1218 <jy331399@dal.ca>
2019-02-05 05:28:48 +0000

    task1 completed

--- l5t1.py
+++ l5t1.py
@@ -1,6 +1,6 @@
 import math
 
-class point:
+class Point:
        def __init__(self):
              self.a = 0
              self.b = 0
@@ -11,17 +11,17 @@ def dist_between_points(x,y):
        dist =math.sqrt(d1**2 + d2**2)
        return dist
 
-a = point()
-b = point()
+a = Point()
+b = Point()
 print(a)
 print(b)
 print(a !=b)
 
-p0 = point()
+p0 = Point()
 p0.a = 14
 p0.b = 18
 
-p1 = point()
+p1 = Point()
 p1.a = 20
 p1.b = 25
 print('distance')
